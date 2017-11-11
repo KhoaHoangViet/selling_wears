8dc0ef5d9b57435f7de4490b9903944fa56bb858
tardigrades <dothisnows@gmail.com>
2017-11-11 14:32:26 +0700

    Add User model and test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171108013120) do
+ActiveRecord::Schema.define(version: 20171111072629) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 20171108013120) do
     t.datetime "updated_at", null: false
     t.string "code"
     t.string "img_url"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
