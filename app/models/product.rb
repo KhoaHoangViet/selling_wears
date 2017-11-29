b9aa22b3e4c2b79b2c80bec1c46b5e36246dbc80
tardigrades <dothisnows@gmail.com>
2017-11-29 21:37:13 +0700

    Implement search box

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,14 @@
 class Product < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: {maximum: 50}
   validates :price, presence: true
-  validates :lable, presence: true, length: { maximum: 20 }
+  validates :lable, presence: true, length: {maximum: 20}
   validates :code, presence: true, uniqueness: true
   # validates_numericality_of :stock, :price, greater_than_or_equal_to: 0
   validates_numericality_of :price, greater_than_or_equal_to: 0
   has_many :cart_items
+
+  def self.search search
+    where("name LIKE ? OR code LIKE ? OR lable LIKE ?", "%#{search}%", 
+      "%#{search}%", "%#{search}%") 
+  end
 end
